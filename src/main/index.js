6b5a2abd2a62e25818aea09bfbf2fa69edb5e853
Raphael Bravo <raphael.bravo@eigital.com>
2019-06-19 17:08:33 -0500

    changes

--- src/main/index.js
+++ src/main/index.js
@@ -1,162 +1,242 @@
-import React, {Component} from 'react';
-import {TextInput,Image, StyleSheet,TouchableOpacity, Text, View, ScrollView} from 'react-native';
+import React, {
+    Component
+} from 'react';
+import {
+    TextInput,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    Text,
+    View,
+    ScrollView
+} from 'react-native';
 import Item from './item';
 import Database from '../services/sqlite_service';
 
-const db =  new Database()
-export default class TodoList extends Component{
-    state={
+const db = new Database()
+export default class TodoList extends Component {
+    state = {
         list: [],
-        val:'',
-        edit:''
+        val: '',
+        edit: ''
     }
-    addItem=()=>{
-        let tempList=this.state.list
+    addItem = () => {
+        let tempList = this.state.list
         //TO ADD
-        if(this.state.val!=''){
-            if(this.state.edit.toString()=='') {
-                
-                db.addProduct({id:tempList.length==0?0:tempList[tempList.length-1].id+1,name:this.state.val,status:false}).then(()=>{
-                   this.setState({
-                    list: [...tempList,{name:this.state.val, status:false}]
-                },()=>{
+        if (this.state.val != '') {
+            if (this.state.edit.toString() == '') {
+
+                db.addProduct({
+                    id: tempList.length == 0 ? 0 : tempList[tempList.length - 1].id + 1,
+                    name: this.state.val,
+                    status: false
+                }).then(() => {
                     this.setState({
-                        val: ''
-                    })  
-                }
-                ) 
+                        list: [...tempList, {
+                            name: this.state.val,
+                            status: false
+                        }]
+                    }, () => {
+                        this.setState({
+                            val: ''
+                        })
+                    })
                 })
-                
+
             } else {
-            //TO EDIT
-                tempList= this.state.list
-                tempList[this.state.edit].name=this.state.val
-                db.updateProduct(this.state.edit,tempList[this.state.edit])
-                this.setState({list:tempList, edit:'', val:''})
+                //TO EDIT
+                tempList = this.state.list
+                tempList[this.state.edit].name = this.state.val
+                db.updateProduct(this.state.edit, tempList[this.state.edit])
+                this.setState({
+                    list: tempList,
+                    edit: '',
+                    val: ''
+                })
             }
-        } 
+        }
     }
-    toggleStatusItem=(index)=>{
+    toggleStatusItem = (index) => {
         //TO MARK AS COMPLETED
-        let tempList= this.state.list
-        tempList[index].status=!tempList[index].status
-        db.updateProduct(index,tempList[index])
-        this.setState({list:tempList})
-    }
-    deleteItem=(index)=>{
-        //TO DELETE
-        let temp= this.state.list.filter((item,i)=>{
-            return i!=index
+        let tempList = this.state.list
+        tempList[index].status = !tempList[index].status
+        db.updateProduct(index, tempList[index])
+        this.setState({
+            list: tempList
         })
-        db.deleteProduct(index).then(()=>{
-            this.setState({list:temp})
-            if(this.state.edit.toString()!=''){
-                this.setState({edit:'', val:''})
+    }
+    deleteItem = (index) => {
+        //TO DELETE
+        let temp = this.state.list.filter((item, i) => {
+            return i != index
+        })
+        db.deleteProduct(index).then(() => {
+            this.setState({
+                list: temp
+            })
+            if (this.state.edit.toString() != '') {
+                this.setState({
+                    edit: '',
+                    val: ''
+                })
             }
         })
-        
+
     }
-    editItem=(index)=>{
+    editItem = (index) => {
         //TO START EDIT
-        this.setState({edit:index, val:this.state.list[index].name})
+        this.setState({
+            edit: index,
+            val: this.state.list[index].name
+        })
     }
-    componentDidMount(){
-        db.listProduct().then(res=>{
+    componentDidMount() {
+        db.listProduct().then(res => {
             //alert(JSON.stringify(res))
-            this.setState({list:res})
-        }).catch(error=>alert(error))
+            this.setState({
+                list: res
+            })
+        }).catch(error => alert(error))
         this.input.focus()
     }
     render() {
-        return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.title}>My To-Do List</Text>
-            </View>
-            <View style={styles.inputContainer}>
-                <TextInput 
-                    autoCorrect={false} 
-                    onSubmitEditing={this.addItem} 
-                    ref={x=>this.input=x} 
-                    clearButtonMode="while-editing" 
-                    style={styles.inputStyle} 
-                    value={this.state.val} 
-                    onChangeText={(val)=>this.setState({val})}/>
-                <TouchableOpacity 
-                    onPress={this.addItem} 
-                    style={styles.addButtonStyle}>
-                    <Image 
-                        source={require('../assets/add.png')} 
-                        style={styles.addImageStyle}/>
-                </TouchableOpacity>
-            </View>
-            <ScrollView 
-                keyboardDismissMode={true} 
-                keyboardShouldPersistTaps="always" 
-                style={styles.scrollContainer}>
-                {
-                    this.state.list.map((item,i)=>{
-                        return <Item key={i} 
-                                    editItem={()=>this.editItem(i)} 
-                                    deleteItem={()=>this.deleteItem(i)} 
-                                    toggleStatusItem={()=>this.toggleStatusItem(i)} 
-                                    name={item.name} 
-                                    status={item.status}/>
-                    })
-                }
-            </ScrollView>
-        </View>
+        return ( <
+            View style = {
+                styles.container
+            } >
+            <
+            View style = {
+                styles.header
+            } >
+            <
+            Text style = {
+                styles.title
+            } > My To - Do List < /Text> <
+            /View> <
+            View style = {
+                styles.inputContainer
+            } >
+            <
+            TextInput autoCorrect = {
+                false
+            }
+            onSubmitEditing = {
+                this.addItem
+            }
+            ref = {
+                x => this.input = x
+            }
+            clearButtonMode = "while-editing"
+            style = {
+                styles.inputStyle
+            }
+            value = {
+                this.state.val
+            }
+            onChangeText = {
+                (val) => this.setState({
+                    val
+                })
+            }
+            /> <
+            TouchableOpacity onPress = {
+                this.addItem
+            }
+            style = {
+                styles.addButtonStyle
+            } >
+            <
+            Image source = {
+                require('../assets/add.png')
+            }
+            style = {
+                styles.addImageStyle
+            }
+            /> <
+            /TouchableOpacity> <
+            /View> <
+            ScrollView keyboardDismissMode = {
+                true
+            }
+            keyboardShouldPersistTaps = "always"
+            style = {
+                styles.scrollContainer
+            } > {
+                this.state.list.map((item, i) => {
+                    return <Item key = {
+                        i
+                    }
+                    editItem = {
+                        () => this.editItem(i)
+                    }
+                    deleteItem = {
+                        () => this.deleteItem(i)
+                    }
+                    toggleStatusItem = {
+                        () => this.toggleStatusItem(i)
+                    }
+                    name = {
+                        item.name
+                    }
+                    status = {
+                        item.status
+                    }
+                    />
+                })
+            } <
+            /ScrollView> <
+            /View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width:'100%',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  header: {
-    width:'100%', 
-    borderColor:'rgb(100,100,100)',
-    borderBottomWidth:2, 
-    alignItems:'center', 
-    padding:20
-  },
-  title: {
-    fontSize:20,
-    color:'black',
-    fontFamily:"Montserrat-Bold"
-  },
-  inputContainer: {
-    flexDirection:'row',
-    borderColor:'rgb(100,100,100)',
-    borderBottomWidth:2,
-    alignItems:'flex-end'
-  },
-  inputStyle:{
-    borderColor:'rgb(100,100,100)', 
-    fontSize:20,
-    fontFamily:'Montserrat-Medium',
-    borderBottomWidth:2, 
-    flex:1,
-    marginHorizontal:10,
-    marginBottom:10,
-    paddingVertical:5, 
-    paddingHorizontal:10
-  },
-  addButtonStyle:{
-    padding:20
-  },
-  addImageStyle:{
-    width:30,
-    height:30
-  },
-  scrollContainer:{
-    flex:1, 
-    width:'100%'
-  }
+    container: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    header: {
+        width: '100%',
+        borderColor: 'rgb(100,100,100)',
+        borderBottomWidth: 2,
+        alignItems: 'center',
+        padding: 20
+    },
+    title: {
+        fontSize: 20,
+        color: 'black',
+        fontFamily: "Montserrat-Bold"
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        borderColor: 'rgb(100,100,100)',
+        borderBottomWidth: 2,
+        alignItems: 'flex-end'
+    },
+    inputStyle: {
+        borderColor: 'rgb(100,100,100)',
+        fontSize: 20,
+        fontFamily: 'Montserrat-Medium',
+        borderBottomWidth: 2,
+        flex: 1,
+        marginHorizontal: 10,
+        marginBottom: 10,
+        paddingVertical: 5,
+        paddingHorizontal: 10
+    },
+    addButtonStyle: {
+        padding: 20
+    },
+    addImageStyle: {
+        width: 30,
+        height: 30
+    },
+    scrollContainer: {
+        flex: 1,
+        width: '100%'
+    }
 
 });
